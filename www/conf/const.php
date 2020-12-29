1783f1e42774fde1c9b1be1bded40b0d6a846caa
nmt667r <nm66flat@gmail.com>
2020-12-29 19:25:30 +0900

    ログイン情報のバリデーションを追加しました。

---
+++ www/conf/const.php
@@ -0,0 +1,14 @@
+<?php
+
+//$document_root =$_SERVER['DOCUMENT_ROOT'] .'/vlt/html';
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', '2020ec');
+define('DB_USER', 'testuser');
+define('DB_PASS', 'password');
+define('DB_CHARSET', 'utf8');
+
+define('HOME_PATH', './home.php');
+define('LOGIN_PATH', './login.php');
+
+?>
---
+++ www/conf/error_messages.php
@@ -0,0 +1,8 @@
+<?php
+
+define('EMPTY_NAME', '名前を入力してください');
+define('LENGTH_MIN_NAME', '名前は6文字以上で入力してください');
+define('LENGTH_MAX_NAME', '名前は20文字以下で入力してください');
+define('EMPTY_PASSWORD', 'パスワードを入力してください');
+
+?>
