927a8dfbfc2b580af539aca3eb7e08846a201064
nmt667r <nm66flat@gmail.com>
2021-01-01 19:21:12 +0900

    [update]商品追加処理に個数を増やしました

--- www/conf/error_messages.php
+++ www/conf/error_messages.php
@@ -9,14 +9,14 @@ define('FAILED_LOGIN', '名前またはパスワードが間違っています')
 
 //商品管理画面用
 define('EMPTY_ITEM_NAME',         '商品名を入力してください');
-define('EXCESS_LENGTH_ITEM_NAME', '商品名は以下で入力してください'); //ITEM_NAME_LENGTH_MAXが桁数指定
+define('EXCESS_LENGTH_ITEM_NAME', '商品名は以下で入力してください'); 
 define('REGEXP_SPACE_ONLY_NAME',  '商品名はスペースのみで登録することができません。');
 define('EMPTY_PRICE',         '価格を入力してください');
 define('ILLEGAL_VALUE_PRICE', '価格の値が正しくありません');
 define('OVER_LENGTH_PRICE',   '価格は1億円以下で入力してください');
 define('EMPTY_STOCK',          '個数を入力してください');
 define('ILLEGAL_VALUE_STOCK',  '個数の値が正しくありません。');
-define('OVER_LENGTH_STOCK',    '個数は～未満で入力してください');
+define('OVER_LENGTH_STOCK',    '個数は100000個未満で入力してください');
 define('EMPTY_FILE',          'ファイルを選択してください。');
 define('ILLEGAL_FILE_FORMAT', 'ファイル形式が異なります。画像ファイルはJPEGもしくはPNGのみ利用可能です');
 
