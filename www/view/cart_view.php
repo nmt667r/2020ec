aabc81703c9260abb4c22106f0c1c5c40ce5d8fb
nmt667r <nm66flat@gmail.com>
2021-01-02 16:36:17 +0900

    [fix]カート内にアイテムがない場合にエラーを出してしまう処理を修正

--- www/view/cart_view.php
+++ www/view/cart_view.php
@@ -9,27 +9,29 @@
         <?php foreach ($err_msg as $read) { ?>
             <p><?php print $read; ?></p>
         <?php } ?>
-
-        <?php foreach ($carts as $cart) { ?>
-            <h4><?php print $cart['name'];?></h4>
-            <p><?php print $cart['price'];?>円</p>
-            <p><?php print $cart['amount']; ?>個</p>
-            <p>小計：<?php print $cart['amount'] * $cart['price']; ?>円</p>
-            <!-- <p><img src="<?php print ITEMS_IMG_DIR . $cart['img']; ?>"></p> -->
-            <p>更新日時：<?php print $cart['update_datetime'];?></p>
-            <form method="post">
-                <input type="submit" value="カートから削除">
-                <input type="hidden" name="item_id" value="<?php print $cart['item_id']; ?>"> 
-                <input type="hidden" name="action" value="delete">
-            </form>           
-            <?php $total = $total + $cart['amount'] * $cart['price'];?>
-        <?php } ?>
-        <h4>合計：<?php print $total?>円</h4>
-        <form method="post">
-            <input type="submit" value="購入">  
-            <input type="hidden" name="action" value="buy">
-        </form>
         
-
+        <?php if($carts !== NULL){ ?>        
+            <?php foreach ($carts as $cart) { ?>
+                <h4><?php print $cart['name'];?></h4>
+                <p><?php print $cart['price'];?>円</p>
+                <p><?php print $cart['amount']; ?>個</p>
+                <p>小計：<?php print $cart['amount'] * $cart['price']; ?>円</p>
+                <!-- <p><img src="<?php print ITEMS_IMG_DIR . $cart['img']; ?>"></p> -->
+                <p>更新日時：<?php print $cart['update_datetime'];?></p>
+                <form method="post">
+                    <input type="submit" value="カートから削除">
+                    <input type="hidden" name="item_id" value="<?php print $cart['item_id']; ?>"> 
+                    <input type="hidden" name="action" value="delete">
+                </form>           
+                <?php $total = $total + $cart['amount'] * $cart['price'];?>
+            <?php } ?>
+            <h4>合計：<?php print $total?>円</h4>
+            <form method="post">
+                <input type="submit" value="購入">  
+                <input type="hidden" name="action" value="buy">
+            </form>
+        <?php } else { ?>
+            <p>カート内にアイテムがありません。</p>
+        <?php } ?>
     </body>
 </html>
